a1f94b75be94a4b75bde5735af6358dacc961f62
Rehan <callmedt2005@gmail.com>
2024-03-26 17:56:25 +0600

    1

--- kiree/kiree-frontend/src/pages/Home.js
+++ kiree/kiree-frontend/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>
+    <div className="container home-container">
       <h1>Welcome to Kiree</h1>
       <p>Discover and share amazing photos!</p>
     </div>
